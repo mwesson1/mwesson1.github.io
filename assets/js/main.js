443fdbddc09eed213319a65724b5f4d62a43fc02
mwesson1 <36869818+mwesson1@users.noreply.github.com>
2018-03-13 21:06:03 -0500

    update with coaches bios and testimonials

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,22 @@
 		$banner = $('#banner'),
 		$body = $('body');
 
+
+	// var $form = $('form#test-form'),
+	//     url = 'https://script.google.com/macros/s/AKfycbz2nbUgn-lL1Lyv3Xe4KPOc2HdFqAzt2SknyFIEiUlkoq-RILXs/exec'
+
+	// $('#submit-form').on('click', function(e) {
+	//   e.preventDefault();
+	//   var jqxhr = $.ajax({
+	//     url: url,
+	//     method: "GET",
+	//     dataType: "json",
+	//     data: $form.serializeObject()
+	//   }).success(
+	//     // do something
+	//   );
+	// })
+
 	// Breakpoints.
 		breakpoints({
 			default:   ['1681px',   null       ],
